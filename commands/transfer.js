b306caa8cc0f81a3a3e3c811799af203ec51357b
Sean Simkus <81839655+seansimkus@users.noreply.github.com>
2021-10-16 23:29:08 -0700

    Made reply hidden by default

--- commands/transfer.js
+++ commands/transfer.js
@@ -4,24 +4,28 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('transfer')
         .setDescription('Transfer coins to another player')
+        //Creates option for which player to send currency to
 		.addUserOption(option =>
 			option.setName('recipient')
 				.setDescription('The name of the player you are sending coins to.')
 				.setRequired(true))
+        //Creates option for how much currency to send      
 		.addIntegerOption(option =>
 			option.setName('amount')
 			.setDescription('The amount you would like to send')
 			.setRequired(true))
+        //Creates option to hide the message from chat
         .addBooleanOption(option => 
-            option.setName('hidden')
-                .setDescription('Choose if the response is hidden')
+            option.setName('show')
+                .setDescription('Choose to show the reply')
                 .setRequired(false)),
     async execute(interaction, currency) {
 
 		const currentAmount = currency.getBalance(interaction.user.id);
 		const transferAmount = interaction.options.getInteger('amount');
 		const transferTarget = interaction.options.getUser('recipient');
-        const ephemeral = interaction.options.getBoolean('hidden');
+        const ephemeral = interaction.options.getBoolean('show');
+        
 
 		if (transferAmount > currentAmount) return interaction.reply(`Sorry ${interaction.user} you don't have that much.`);
 		if (transferAmount <= 0) return interaction.reply(`Please enter an amount greater than zero, ${interaction.user}`);
@@ -29,6 +33,6 @@ module.exports = {
 		currency.add(interaction.user.id, -transferAmount);
 		currency.add(transferTarget.id, transferAmount);
 
-		return interaction.reply({content: `Successfully transferred ${transferAmount} 💰 to ${transferTarget.username}. Your current balance is ${currency.getBalance(interaction.user.id)} 💰` , ephemeral: ephemeral});
+		return interaction.reply({content: `Successfully transferred ${transferAmount} 💰 to ${transferTarget.username}. Your current balance is ${currency.getBalance(interaction.user.id)} 💰` , ephemeral: !ephemeral});
     }, //, {ephemeral: interaction.options.getBoolean('visibility')}
 };
