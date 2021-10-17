75fd230260f487a759f0fa3a64bb6dd3d1fdd11a
Sean Simkus <81839655+seansimkus@users.noreply.github.com>
2021-10-16 23:16:03 -0700

    Added option to hide reply

--- commands/transfer.js
+++ commands/transfer.js
@@ -11,16 +11,17 @@ module.exports = {
 		.addIntegerOption(option =>
 			option.setName('amount')
 			.setDescription('The amount you would like to send')
-			.setRequired(true)),
-            // .addBooleanOption(option => 
-            //     option.setName('visibility')
-            //         .setDescription('Choose if the response is hidden')
-            //         .setRequired(false)),
+			.setRequired(true))
+        .addBooleanOption(option => 
+            option.setName('hidden')
+                .setDescription('Choose if the response is hidden')
+                .setRequired(false)),
     async execute(interaction, currency) {
 
 		const currentAmount = currency.getBalance(interaction.user.id);
 		const transferAmount = interaction.options.getInteger('amount');
 		const transferTarget = interaction.options.getUser('recipient');
+        const ephemeral = interaction.options.getBoolean('hidden');
 
 		if (transferAmount > currentAmount) return interaction.reply(`Sorry ${interaction.user} you don't have that much.`);
 		if (transferAmount <= 0) return interaction.reply(`Please enter an amount greater than zero, ${interaction.user}`);
@@ -28,5 +29,6 @@ module.exports = {
 		currency.add(interaction.user.id, -transferAmount);
 		currency.add(transferTarget.id, transferAmount);
 
-		return interaction.reply(`Successfully transferred ${transferAmount} 💰 to ${transferTarget.username}. Your current balance is ${currency.getBalance(interaction.user.id)} 💰`);    }, //, {ephemeral: interaction.options.getBoolean('visibility')}
+		return interaction.reply({content: `Successfully transferred ${transferAmount} 💰 to ${transferTarget.username}. Your current balance is ${currency.getBalance(interaction.user.id)} 💰` , ephemeral: ephemeral});
+    }, //, {ephemeral: interaction.options.getBoolean('visibility')}
 };
